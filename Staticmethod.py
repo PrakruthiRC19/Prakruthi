1e7bee929edb5517a2d11bbdeb2b3b304272423f
PrakruthiRC19 <prakruthichannaiah@gmail.com>
2026-02-24 10:45:58 +0530

    Add files via upload

---
+++ Staticmethod.py
@@ -0,0 +1,38 @@
+class DMART:
+
+    discountOfEachProduct=0.01
+
+    def __init__(self):
+        self.gstn=None
+        self.location=None
+
+    def calculatePrice(self, prodName, originalPriceOfProduct):
+        actualPrice=originalPriceOfProduct-(originalPriceOfProduct*DMART.discountOfEachProduct)
+        print("Actual price of",prodName , "after discount",actualPrice)
+
+    @classmethod
+    def revisedDiscount(cls,newDiscount):
+        cls.discountOfEachProduct=newDiscount
+
+    @staticmethod
+    def friskTheCustomer():
+        print("Customer are checked and validated")
+
+print("***Execution of static function***")
+DMART.friskTheCustomer()
+
+print("***PRICE OF DEFAULT DISCOUNT***")
+d1=DMART()
+d1.gstn=12345678
+d1.location="BTM"
+d1.calculatePrice("prestige cooker",5000)
+
+d2=DMART()
+d2.gstn=12345678
+d2.location="BTM"
+d2.calculatePrice("philips airflyer",8000)
+
+DMART.revisedDiscount(0.20)
+print("***PRICE WITH REVISED DISCOUNT DUE TO FESTIVAL***")
+d1.calculatePrice("prestige cooker",5000)
+d2.calculatePrice("philips airflyer",8000)
