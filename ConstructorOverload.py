de3ef922544c85d34d6324e284502c2bc760c8a6
PrakruthiRC19 <prakruthichannaiah@gmail.com>
2026-03-01 16:35:12 +0530

    Add files via upload

---
+++ ConstructorOverload.py
@@ -0,0 +1,9 @@
+class Calculate:
+
+    def __init__(self):
+        print("CONSTRUCTOR WITHOUT PARAMETER")
+
+    def __init__(self, a,b):
+        print("CONSTRUCTOR WITH PARAMETER")
+
+c=Calculate(10,20)
---
+++ MethodOverloaded.py
@@ -0,0 +1,20 @@
+class Calculate:
+
+    def add(self):
+        x=10
+        y=20
+        z=x+y
+        print(z)
+
+    def add(self,a,b):
+        return (a+b) # if we want to return the statement , we need to invote retrurn in print statement.
+                     # assign method caller to variable and invoke that variable.
+    
+    def add(self,x,y):
+        a=x
+        b=y
+        c=a+b
+        print(c)
+    
+c=Calculate()
+c.add(10,20)
