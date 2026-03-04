68c5d8c3417cdd42a12560025974a807bb719668
PrakruthiRC19 <prakruthichannaiah@gmail.com>
2026-03-04 09:20:48 +0530

    Delete BankAccount.py

--- BankAccount.py
+++
@@ -1,9 +0,0 @@
-class BankAccount:
-
-    def __init__(self,name,contact,age):
-        self.acctHolderName=name
-        self.acctHolderContact=contact
-        self.acctHolderAge=age
-
-b1=BankAccount("John",123456789,28)
-print(b1.__dict__) # {'acctHolderName': 'John', 'acctHolderContact': 123456789, 'acctHolderAge': 28} directly we can print using object ref dict.
