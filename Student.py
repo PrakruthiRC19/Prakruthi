e667c4b856a36c8bb2f65eb70de860deb8d3cfc1
PrakruthiRC19 <prakruthichannaiah@gmail.com>
2026-03-04 09:10:33 +0530

    Add files via upload

--- Student.py
+++ Student.py
@@ -1,34 +1,9 @@
 class Student:
+
     def __init__(self):
-        self.rollNum=None
-        self.name=None
+        self.sId=101
+        self.sName="John"
+
 s1=Student()
-print("*****BEFORE VARIABLE DATA ASSIGN*****")
-print(s1.rollNum)
-print(s1.name)
-
-print("*****AFTER VARIABLE DATA ASSIGN*****")
-s1.rollNum=101
-s1.name="Jhon"
-
-print(s1.rollNum)
-print(s1.name)
-
-s2=Student()
-print("*****BEFORE VARIABLE DATA ASSIGN*****")
-print(s2.rollNum)
-print(s2.name)
-
-print("*****AFTER VARIABLE DATA ASSIGN*****")
-s2.rollNum=102
-s2.name="David"
-
-print(s2.rollNum)
-print(s2.name)
-
-s3=Student()
-print("*****VARIABLE DATA IS NOT ASSIGN*****")
-print(s3.rollNum)
-print(s3.name)
-
-
+print(s1.sId,s1.sName) # or we can directly print the instance variable using object reference dictionary
+print(s1.__dict__)
