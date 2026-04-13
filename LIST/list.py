7cc05ac23fa02dfae5c56d01e3efbd3a49fc9fbc
PrakruthiRC19 <prakruthichannaiah@gmail.com>
2026-04-13 10:53:07 +0530

    Add files via upload

---
+++ LIST/list.py
@@ -0,0 +1,37 @@
+x=[1,2,3,4] # 1st way
+print(x)
+
+y=list() # 2nd way 
+y.append(1)
+y.append(2)
+y.append(3)
+y.append(4)
+print(y)
+
+z=[]
+z.append(1)
+z.append(2)
+print(z)
+
+# indexing
+
+print("INDEXING")
+#  0,1,2,3,4,5,6,7,8,9
+a=[1,2,3,4,5,6,7,8,9,10]
+print(a[4])
+
+# duplicate elements
+
+print("DUPLICATE ELEMENTS")
+b=[1,2,3,4,5,6,7,8,9,10,1,2,3,4,5]
+print(b)
+
+# none elments
+
+c=[1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,None]
+print(c)
+
+# multiple none 
+
+d=[1,2,3,4,5,6,7,8,9,None,None,None]
+print(d)
