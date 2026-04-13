518c1d78e082bfe97d1c52b7d0b395229aee38e5
PrakruthiRC19 <prakruthichannaiah@gmail.com>
2026-04-13 10:56:54 +0530

    Add files via upload

---
+++ METHODS OF LIST CLASS/nestedlist.py
@@ -0,0 +1,16 @@
+print("NESTED LIST - 1 LIST OBJ PRESENT INSIDE ANOTHER LIST OBJ")
+
+x=[1,2,3,4]
+print(x)
+
+y=['a','b','c']
+
+x.append(y)
+
+print(x) 
+
+print(y)
+
+print(x[4][0])
+print(x[4][1])
+print(x[4][2])
