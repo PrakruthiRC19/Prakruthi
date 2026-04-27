f47301e772a015c22a945e62443c136088bbb1f9
PrakruthiRC19 <prakruthichannaiah@gmail.com>
2026-04-27 17:41:15 +0530

    Add files via upload

---
+++ dictionary and string.py
@@ -0,0 +1,20 @@
+# DUCTIONARY
+
+student = {
+    "name": "Prakruthi",
+    "age": 21,
+    "course": "Python"
+}
+
+print(student)
+
+print(student["name"])
+print(student["age"])
+
+# STRING 
+
+name = "Python Programming"
+print(name)
+
+print(name[0])   # First character
+print(name[-1])  # Last character
