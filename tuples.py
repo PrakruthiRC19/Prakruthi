33c1206b1c7fa33e470e4a8fe318bfcea692b4a5
PrakruthiRC19 <prakruthichannaiah@gmail.com>
2026-04-27 17:36:17 +0530

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,11 @@
+student = ("Prakruthi", 21, "Python")
+print(student)
+
+colors = ("red", "blue", "green")
+print(colors[0])   # Access first element
+
+'''numbers = (10, 20, 30)
+numbers[0] = 100   # Error'''
+
+single = (5,)
+print(type(single))
