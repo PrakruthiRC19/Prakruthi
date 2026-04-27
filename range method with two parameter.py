6460dbe425f1becfe9241ebff07fbc73494fe1c5
PrakruthiRC19 <prakruthichannaiah@gmail.com>
2026-04-27 17:45:31 +0530

    Add files via upload

---
+++ range method with two parameter.py
@@ -0,0 +1,17 @@
+for i in range(0,6):
+    print(i)
+
+print("\nDifference\n")
+
+for i in range(2,10):
+    print(i) 
+
+print("\n Negative no\n")
+
+for i in range(-5,5):
+    print(i)
+
+print("\nIf we give negative value for cond\n")
+
+for i in range(5,-5):
+    print(i)
